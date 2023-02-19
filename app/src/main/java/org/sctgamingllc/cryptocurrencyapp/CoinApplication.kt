c6b050668c503a690b0736f382cd07c37ac21d32
stavares <stavares@thecityside.com>
2023-02-19 16:34:56 -0500

    Change the top level module name by modifying the 'settings.gradle' file in the root of the project directory structure.

--- app/src/main/java/org/sctgamingllc/cryptocurrencyapp/CoinApplication.kt
+++ app/src/main/java/org/sctgamingllc/cryptocurrencyapp/CoinApplication.kt
@@ -1,17 +1,5 @@
 package org.sctgamingllc.cryptocurrencyapp
 
-//import android.app.Application
-//import android.content.Intent
-//import dagger.hilt.android.HiltAndroidApp
-//
-//@HiltAndroidApp
-//class CoinApplication: Application() {
-////    val start = MainActivity()
-//    override fun startActivity(intent: Intent?) {
-//        super.startActivity(intent)
-//    }
-//}
-
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
